b4e70d9db31b33cc3bedddf5c90f90c6bfa6e179
berlin-n <130678445+berlin-n@users.noreply.github.com>
2024-09-25 17:57:34 +0100

    In a function, Initialize a dictionary of lists to identify file types by their extensions

--- File Organization/File Organization Script.py
+++ File Organization/File Organization Script.py
@@ -1,3 +1,15 @@
 import os # library for intercating with the opearating system
 import shutil # shutil is a library for high level file operations
 
+def organize_folder(folder_path):
+    file_types = {
+        'Images': ['.png','.jpg','.jpeg','.gif'],
+        'Documents': ['.pdf', '.xlxs', '.docx', '.txt'],
+        'Videos': ['.mp4', '.mkv', '.avi', '.mov'],
+        'Music': ['.mp3', '.wav']
+    }
+
+
+        
+    
+
